e50c288a9172470441fd4a1ab8ac2f7c086d9a21
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-07-07 17:51:15 -0300

    Intercepting http request with angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientInMemoryWebApiModule, InMemoryDbService} from 'angular-in-memory-web-api';
+import {inMemoryDatabase } from './in-memory-database';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(inMemoryDatabase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -3,6 +3,5 @@ export class Category {
     public id?:number,
     public name?:string,
     public description?:string,
-
   ){}
 }
